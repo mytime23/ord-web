2eda1fd7a6ccae3117a97516b42c1c4d5a312dad
Tim <ord365@manstrong.com.tw>
2019-02-01 09:25:15 +0800

    Updates

---
+++ precache-manifest.51b797a609396fdf517ee25e23afb277.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ord-web/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9509b7f1b3044926d253",
+    "url": "/ord-web/static/js/runtime~main.9509b7f1.js"
+  },
+  {
+    "revision": "d0f2c6ceb668dc65206c",
+    "url": "/ord-web/static/js/main.d0f2c6ce.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/ord-web/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "d0f2c6ceb668dc65206c",
+    "url": "/ord-web/static/css/main.90bcd8e3.chunk.css"
+  },
+  {
+    "revision": "dd0258546300761683b5c451803cef82",
+    "url": "/ord-web/index.html"
+  }
+];
